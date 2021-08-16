f409f2049d5edfe008270ffc45ca1de56c24ba87
nelsonchege <68642235+nelsonchege@users.noreply.github.com>
2021-08-16 17:22:08 +0300

    Delete New folder/makeandbake directory

--- New folder/makeandbake/cake_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CakeVariety , DisplayCakes , CakeOrder,Testdb
-# Register your models here.
-admin.site.register(CakeVariety)
-admin.site.register(DisplayCakes)
-admin.site.register(CakeOrder)
-admin.site.register(Testdb)
