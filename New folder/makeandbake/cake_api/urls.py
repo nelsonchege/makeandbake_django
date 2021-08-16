17beec127deb848d0bc3365c4bacdb331f1304a7
nelsonchege <nelson3chege@gmail.com>
2021-08-16 17:25:38 +0300

    new

--- New folder/makeandbake/cake_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CakeVariety, DisplayCakes, CakeOrder
-
-urlpatterns = [
-    path('displaycakes',DisplayCakes.as_view()),
-    path('cakeorder',CakeOrder.as_view()),
-    path('cakeorder/<int:pk>/',CakeOrder.as_view()),
-    path('cakevariety',CakeVariety.as_view())
-]
